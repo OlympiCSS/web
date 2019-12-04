804ca12ca54a87152947061d57744a5e41dfb900
César Alberca <cesalberca@gmail.com>
2019-12-04 18:22:28 +0100

    Add jest configuration

---
+++ src/domain/utils/maybe.ts
@@ -0,0 +1,58 @@
+export class Maybe<T> {
+  private constructor(private value: T | null) {}
+
+  static from<T>(value: T | undefined | null): Maybe<T> {
+    return this.isValid(value) ? Maybe.some(value as T) : Maybe.none<T>()
+  }
+
+  static none<T>(): Maybe<T> {
+    return new Maybe<T>(null)
+  }
+
+  private static some<T>(value: T): Maybe<T> {
+    if (!this.isValid(value)) {
+      throw new Error('Provided value must not be empty')
+    }
+    return new Maybe(value)
+  }
+
+  private static isValid(value: unknown | null | undefined): boolean {
+    return !!value || this.isNumberZero(value) || this.isFalse(value)
+  }
+
+  private static isNumberZero<R>(value: R): boolean {
+    return typeof value === 'number' && value === 0
+  }
+
+  private static isFalse<R>(value: R): boolean {
+    return typeof value === 'boolean' && !value
+  }
+
+  get(defaultValue: T): T {
+    return this.value === null ? defaultValue : this.value
+  }
+
+  map<R>(f: (wrapped: T) => R): Maybe<R> {
+    if (this.value === null) {
+      return Maybe.none<R>()
+    } else {
+      return Maybe.some(f(this.value))
+    }
+  }
+
+  flatMap<R>(f: (wrapped: T) => Maybe<R>): Maybe<R> {
+    if (this.value === null) {
+      return Maybe.none<R>()
+    } else {
+      return f(this.value)
+    }
+  }
+
+  getOrThrow(error: Error): T {
+    return this.value === null
+      ? (() => {
+          throw error
+        })()
+      : this.value
+  }
+}
--- src/ui/app.ts
+++ src/ui/app.ts
@@ -1,7 +1,7 @@
 import { customElement, html, LitElement } from 'lit-element'
 
 @customElement('olympicss-app')
-export class OlympicssApp extends LitElement {
+export class App extends LitElement {
   render() {
     return html`
       <p>Hello</p>
